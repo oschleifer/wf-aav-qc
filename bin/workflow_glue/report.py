75ee04a37dfc1d4a5ab98ab34ec5e28dc3a98910
Olivia Schleifer <oschleifer@g.hmc.edu>
2024-06-18 16:22:56 -0700

    parse df

--- bin/workflow_glue/report.py
+++ bin/workflow_glue/report.py
@@ -123,7 +123,8 @@ def plot_contamination(report, class_counts):
                 df_alns = pd.concat([df_alns, df_sample[~df_sample.Reference.isin(['Mapped', 'Unmapped'])]])
             df_reads = df_reads.rename(columns={'Percentage of alignments': 'Percentage of Reads'})
             plt = ezc.barplot(
-                df_reads[['Reference', 'Percentage of Reads']])
+                df_reads[0,['Reference', 'Percentage of Reads']],
+                df_reads[1])
             plt.title = dict(text='Reads mapped/unmapped')
             EZChart(plt, theme='epi2melabs', height='400px')
 
