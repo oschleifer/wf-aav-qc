dd2efebcaf3db09a552eed1c597995dae9159ec0
Olivia Schleifer <oschleifer@g.hmc.edu>
2024-06-20 13:24:27 -0700

    testing plot.add

--- bin/workflow_glue/report.py
+++ bin/workflow_glue/report.py
@@ -119,7 +119,7 @@ def plot_contamination(report, class_counts):
             df_reads = pd.DataFrame()
             df_alns = pd.DataFrame()
             for sample, df_sample in df_class_counts.groupby('sample_id'):
-                df_reads = pd.concat([df_reads, df_sample[df_sample.Reference.isin(['Mapped', 'Unmapped'], index=[sample])]])
+                df_reads = pd.concat([df_reads, df_sample[df_sample.Reference.isin(['Mapped', 'Unmapped'])]])
                 df_alns = pd.concat([df_alns, df_sample[~df_sample.Reference.isin(['Mapped', 'Unmapped'])]])
                 # plt = ezc.barplot(data=df_reads,x='Reference', y='Percentage of Reads', hue=sample)
             df_reads = df_reads.rename(columns={'Percentage of alignments': 'Percentage of Reads'})
