467388c966acd43ea514cd6bcd35e61fdb20fe70
Olivia Schleifer <oschleifer@g.hmc.edu>
2024-06-28 13:00:51 -0700

    fix dataframe type

--- bin/workflow_glue/report.py
+++ bin/workflow_glue/report.py
@@ -214,7 +214,7 @@ def plot_read_summary(report, stats):
 
             # Line graph of base yield
             # Create an empty DataFrame for all samples
-            combined_df = pd.DataFrame
+            combined_df = pd.DataFrame()
 
             for sample_name, df_sample in df_stats.groupby('sample_name'):
                 length = np.concatenate(([0], np.sort(df_sample["read_length"])), dtype="int")
